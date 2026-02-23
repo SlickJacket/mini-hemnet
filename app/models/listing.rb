36a4c6fa4632435d95f9dd2ab14f7272ece86f99
Bobby BuffaloBoy <49796063+SlickJacket@users.noreply.github.com>
2026-02-23 11:21:23 +0100

    Add model validations for Listing and enforce data integrity (#5)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,12 @@
 class Listing < ApplicationRecord
     has_many :insights, dependent: :destroy
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :address, presence: true
+    validates :measurement, numericality: { greater_than: 0 }, allow_nil: true
+    validates :rooms, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+    validates :bathrooms, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+    validates :floors, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
